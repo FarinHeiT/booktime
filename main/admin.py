ed7e8d94515c034520fe71aa78cf95b54fa359c7
FarinHeiT <yariksuharik85@gmail.com>
2019-11-04 21:16:45 +0200

    Bulk Products make_active/make_inactive actions

--- main/admin.py
+++ main/admin.py
@@ -8,6 +8,11 @@ from django.db.models import Avg, Count, Min, Sum
 from django.urls import path
 from django.template.response import TemplateResponse
 from django import forms
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from weasyprint import HTML
+import tempfile
 
 from . import models
 
@@ -389,7 +394,52 @@ class ReportingColoredAdminSite(ColoredAdminSite):
         return super().index(request, extra_context)
 
 
-class OwnersAdminSite(ReportingColoredAdminSite):
+class InvoiceMixin:
+    def get_urls(self):
+        urls = super().get_urls()
+        my_urls = [
+            path(
+                'invoice/<int:order_id>/',
+                self.admin_view(self.invoice_for_order),
+                name='invoice',
+            )
+        ]
+        return my_urls + urls
+
+    def invoice_for_order(self, request, order_id):
+        order = get_object_or_404(models.Order, pk=order_id)
+
+        if request.GET.get('format') == 'pdf':
+            html_string = render_to_string(
+                'invoice.html', {'order': order}
+            )
+            html = HTML(
+                string=html_string,
+                base_url=request.build_absolute_uri(),
+            )
+
+            result = html.write_pdf()
+
+            response = HttpResponse(
+                content_type='application/pdf'
+            )
+            response['Content-Disposition'] = 'inline; filename=invoice.pdf'
+            response['Content-Transfer-Encoding'] = 'binary'
+            with tempfile.NamedTemporaryFile(delete=True) as output:
+                output.write(result)
+                output.flush()
+                output = open(output.name, 'rb')
+                binary_pdf = output.read()
+                response.write(binary_pdf)
+
+            return response
+
+        return render(request, 'invoice.html', {'order': order})
+
+
+class OwnersAdminSite(
+    InvoiceMixin, ReportingColoredAdminSite
+):
     site_header = 'BookTime owners administration'
     site_header_color = 'black'
     module_caption_color = 'grey'
@@ -400,7 +450,9 @@ class OwnersAdminSite(ReportingColoredAdminSite):
         )
 
 
-class CentralOfficeAdminSite(ReportingColoredAdminSite):
+class CentralOfficeAdminSite(
+    InvoiceMixin, ReportingColoredAdminSite
+):
     site_header = 'BookTime central office administration'
     site_header_color = 'purple'
     module_caption_color = 'pink'
